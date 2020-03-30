58cf26e8ea68f661eb26d1d7f8c283abbe5193fc
Junior Marques <junioralcant111@gmail.com>
2020-03-30 10:49:51 -0300

    Listando pedidos

--- src/pages/Pedidos/index.js
+++ src/pages/Pedidos/index.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { parseFromTimeZone, formatToTimeZone } from "date-fns-timezone";
+
 import NavBar from "../../components/NavBar";
 import Footer from "../../components/Footer";
+
+import { formatPrice } from "../../util/formart";
+
+import api from "../../services/api";
 
 import "./styles.css";
 
 export default function Pedidos({ ...props }) {
+  const [pedidos, setPedidos] = useState([]);
+
+  useEffect(() => {
+    async function loadProduto() {
+      const response = await api.get("/pedidos");
+
+      setPedidos(response.data);
+    }
+
+    loadProduto();
+  }, []);
+
+  console.log(pedidos);
+
   return (
     <>
       <div className="body" style={{ paddingBottom: "84%" }}>
@@ -13,97 +33,51 @@ export default function Pedidos({ ...props }) {
           <span className="title">Pedidos</span>
         </div>
 
-        <div className="container col-md-12 container-pedido">
-          <div className="data-pedido col-md-12">
-            <strong>15-04-2020</strong>
-          </div>
-          <div className="pedido">
-            <div className="produtos-desc">
-              <strong>Baiana</strong> <small>12:00 R$</small>
-            </div>
-            <div className="descricao">
-              <small>
-                Prezunto, queijo, milhor, calabresa , queijo, milhor, calabresa
-              </small>
-            </div>
-            <div className="footer-desc">
-              <div>
-                <small>Valor:</small> <strong>22.00 R$</strong>
-              </div>
-              <div className="valor-pedido">
-                <small>QTD:</small> <p style={{ marginLeft: 15 }}>2</p>
-              </div>
-            </div>
-          </div>
-          <div className="pedido">
-            <div className="produtos-desc">
-              <strong>Baiana</strong> <small>12:00 R$</small>
-            </div>
-            <div className="descricao">
-              <small>
-                Prezunto, queijo, milhor, calabresa , queijo, milhor, calabresa
-              </small>
-            </div>
-            <div className="footer-desc">
-              <div>
-                <small>Valor:</small> <strong>22.00 R$</strong>
-              </div>
-              <div className="valor-pedido">
-                <small>QTD:</small> <p style={{ marginLeft: 15 }}>2</p>
-              </div>
-            </div>
-          </div>
+        {pedidos.map(pedido => {
+          const data = parseFromTimeZone(pedido.createdAt, {
+            timeZone: "America/Sao_Paulo"
+          });
 
-          <div className="total-pedido col-md-12">
-            <small>Total:</small> <strong>40.00 R$</strong>
-          </div>
-        </div>
+          const dataFormatada = formatToTimeZone(data, "DD/MM/YYYY", {
+            timeZone: "Europe/Berlin"
+          });
+          return (
+            <div
+              key={pedido._id}
+              className="container col-md-12 container-pedido"
+            >
+              <div className="data-pedido col-md-12">
+                <strong>{dataFormatada}</strong>
+              </div>
+              {pedido.produto.map(p => (
+                <div key={p._id} className="pedido">
+                  <div className="produtos-desc">
+                    <strong>{p.produtoId.nome}</strong>{" "}
+                    <small>{formatPrice(p.produtoId.preco)}</small>
+                  </div>
+                  <div className="descricao">
+                    <small>{p.produtoId.descricao}</small>
+                  </div>
+                  <div className="footer-desc">
+                    <div>
+                      <small>Valor:</small>{" "}
+                      <strong>{formatPrice(p.valor)}</strong>
+                    </div>
+                    <div className="valor-pedido">
+                      <small>QTD:</small>{" "}
+                      <strong style={{ marginLeft: 15 }}>{p.quantidade}</strong>
+                    </div>
+                  </div>
+                </div>
+              ))}
 
-        <div className="container col-md-12 container-pedido">
-          <div className="data-pedido col-md-12">
-            <strong>15-04-2020</strong>
-          </div>
-          <div className="pedido">
-            <div className="produtos-desc">
-              <strong>Baiana</strong> <small>12:00 R$</small>
-            </div>
-            <div className="descricao">
-              <small>
-                Prezunto, queijo, milhor, calabresa , queijo, milhor, calabresa
-              </small>
-            </div>
-            <div className="footer-desc">
-              <div>
-                <small>Valor:</small> <strong>22.00 R$</strong>
-              </div>
-              <div className="valor-pedido">
-                <small>QTD:</small> <p style={{ marginLeft: 15 }}>2</p>
+              <div className="total-pedido col-md-12">
+                <small>Total:</small>{" "}
+                <strong>{formatPrice(pedido.valorTotal)}</strong>
               </div>
             </div>
-          </div>
-          <div className="pedido">
-            <div className="produtos-desc">
-              <strong>Baiana</strong> <small>12:00 R$</small>
-            </div>
-            <div className="descricao">
-              <small>
-                Prezunto, queijo, milhor, calabresa , queijo, milhor, calabresa
-              </small>
-            </div>
-            <div className="footer-desc">
-              <div>
-                <small>Valor:</small> <strong>22.00 R$</strong>
-              </div>
-              <div className="valor-pedido">
-                <small>QTD:</small> <p style={{ marginLeft: 15 }}>2</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="total-pedido col-md-12">
-            <small>Total:</small> <strong>40.00 R$</strong>
-          </div>
-        </div>
+          );
+        })}
       </div>
       <Footer />
     </>
