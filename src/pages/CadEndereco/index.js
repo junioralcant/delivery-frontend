b02852f8bd34b93c38ee81110746005dcfc6475f
Junior Marques <junioralcant111@gmail.com>
2020-03-30 16:29:21 -0300

    Cadastro de enderecos

--- src/pages/CadEndereco/index.js
+++ src/pages/CadEndereco/index.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 import NavBar from "../../components/NavBar";
 import Footer from "../../components/Footer";
 
+import api from "../../services/api";
+
 import "./styles.css";
 
 export default function Cadastrese({ ...props }) {
+  const [rua, setRua] = useState("");
+  const [numero, setNumero] = useState("");
+  const [bairro, setBairro] = useState("");
+
+  async function handlerSubmit(e) {
+    e.preventDefault();
+
+    await api.post("/enderecos", {
+      endereco: {
+        rua: rua,
+        bairro: bairro,
+        numeroCasa: numero
+      }
+    });
+
+    setRua("");
+    setNumero("");
+    setBairro("");
+    props.history.push("/endereco");
+  }
+
   return (
     <>
       <div className="body-cad">
@@ -15,40 +38,44 @@ export default function Cadastrese({ ...props }) {
           <div className="container-title col-md-12">
             <span className="title">Cadastro de endereço</span>
           </div>
-          <form>
-            <div class="form-group">
+          <form onSubmit={handlerSubmit}>
+            <div className="form-group">
               <label>Rua:</label>
-              <input name="rua" placeholder="Rua" class="form-control" />
-            </div>
-            <div class="form-group">
-              <label>Número da residência:</label>
               <input
-                name="numeroCasa"
-                placeholder="Número da residência"
-                class="form-control"
+                onChange={e => setRua(e.target.value)}
+                name="rua"
+                placeholder="Rua"
+                className="form-control"
+                value={rua}
               />
             </div>
-            <div class="form-group">
+            <div className="form-group">
+              <label>Número da residência:</label>
+              <input
+                onChange={e => setNumero(e.target.value)}
+                name="numeroCasa"
+                placeholder="Número da residência"
+                className="form-control"
+                value={numero}
+              />
+            </div>
+            <div className="form-group">
               <label>Bairro:</label>
-              <input name="bairro" placeholder="Bairro" class="form-control" />
+              <input
+                onChange={e => setBairro(e.target.value)}
+                name="bairro"
+                placeholder="Bairro"
+                className="form-control"
+                value={bairro}
+              />
             </div>
 
-            <div class="form-group">
-              <label>Cidade:</label>
-              <input name="cidade" placeholder="Cidade" class="form-control" />
-            </div>
-
-            <div class="form-group">
-              <label>Estado:</label>
-              <input name="estado" placeholder="Estado" class="form-control" />
-            </div>
-
-            <button type="submit" class="btn-lg btn-primary">
+            <button type="submit" className="btn-lg btn-primary">
               Salvar
             </button>
             <button
               type="submit"
-              class="btn-lg btn-danger"
+              className="btn-lg btn-danger"
               style={{ marginLeft: 10 }}
               onClick={() => props.history.push("/endereco")}
             >
