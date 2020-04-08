92ee979760dcc16e270249b69f8ec1516095855f
Junior Marques <junioralcant111@gmail.com>
2020-04-08 10:24:51 -0300

    Configuracao de variavel ambiente

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getToken } from "./auth";
 const api = axios.create({
-  baseURL: "http://172.17.0.1:3001"
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 api.interceptors.request.use(async config => {
