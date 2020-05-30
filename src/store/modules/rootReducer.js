24a8b6ad763b631172032f87cadfc6f9fd39c4fe
junioralcant <junioralcant111@gmail.com>
2020-05-30 11:06:39 -0300

    Importando redurcer de change em rootRedurcer

--- src/store/modules/rootReducer.js
+++ src/store/modules/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
 import cart from "./cart/redurcer";
+import change from "./change/redurcer";
 
 export default combineReducers({
-  cart
+  cart,
+  change,
 });
